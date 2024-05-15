28228cc6a74843fd773350e71a893b3fc9c599b4
djerfy <djerfy@gmail.com>
2024-05-15 14:02:57 +0200

    misc: specify client
    Signed-off-by: djerfy <djerfy@gmail.com>

--- chiabot/client.py
+++ chiabot/client.py
@@ -5,17 +5,15 @@ logger = logging.getLogger('client')
 
 
 class ChiaBotClient(discord.Client):
-
     def __init__(self, *args, plugins=None, **kwargs):
         self.plugins = plugins
         super().__init__(*args, **kwargs)
 
     async def on_ready(self):
-        logger.info(f'We have logged in as {self.user}')
+        logger.info(f'We have logged in as {self.user} (client)')
         await self.plugins.on_ready(self)
 
     async def on_message(self, message):
         if message.author == self.user:
             return
-
         await self.plugins.on_message(self, message)
